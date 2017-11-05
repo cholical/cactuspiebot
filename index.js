6bb540befe59abea6930d968cd3074875f5ad2b8
macvogelsang <macvogelsang@gmail.com>
2017-11-05 03:30:57 +0000

    fixed buffer printing

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var auth = require('./auth.json');
 var zerorpc = require("zerorpc");
 
 var bot = new Discord.Client({
-	token: auth.token,
-	autorun: true
+    token: auth.token,
+    autorun: true
 });
 
 var host = "127.0.0.1";
@@ -16,55 +16,57 @@ var port = 8000;
 var client = new zerorpc.Client();
 client.connect("tcp://" + host + ":" + port);
 
+
 bot.on('ready', function (event) {
-	console.log('Connected');
-	console.log('Logged in as: ');
-	console.log(bot.username + ' - ' + bot.id);
+    console.log('Connected');
+    console.log('Logged in as: ');
+    console.log(bot.username + ' - ' + bot.id);
 
+    var options = {
+        url: 'https://discordapp.com/api/channels/220307845483069440/messages?limit=100',
+        headers: {
+            'User-Agent':'request',
+            'Authorization': bot.internals.token,
+            'Content-type': 'application/json'
+        }
+    }
 
-	var options = {
-		url: 'https://discordapp.com/api/channels/220307845483069440/messages?limit=100',
-		headers: {
-			'User-Agent':'request',
-			'Authorization': bot.internals.token,
-			'Content-type': 'application/json'
-		}
-	}
+    var messages = [];
+    var firstTime = true;
+    var done = true;
 
-	var messages = [];
-	var firstTime = true;
-	var done = true;
-
-	do {
-		var res = sync('get', options.url, options);
-		console.log(res);
-		messages = JSON.parse(res.getBody().toString());
-		_.forEach(messages, function (message) {
-			var line = message.author.username + ' (' + message.timestamp + ') ' + ': ' + message.content + '\n';
-			fs.appendFile('messages.txt', line);
-		});
-		options.url = 'https://discordapp.com/api/channels/220307845483069440/messages?before=' + messages[messages.length - 1].id + '&limit=100';
-	} while (messages.length > 0);
+    do {
+        var res = sync('get', options.url, options);
+        messages = JSON.parse(res.getBody().toString());
+        //console.log(res.getBody().toString());
+        console.log("GET " + messages.length + " messages");
+        _.forEach(messages, function (message) {
+            var line = message.author.username + ' (' + message.timestamp + ') ' + ': ' + message.content + '\n';
+            fs.appendFile('messages.txt', line);
+            //console.log(message.content);
+        });
+        options.url = 'https://discordapp.com/api/channels/220307845483069440/messages?before=' + messages[messages.length - 1].id + '&limit=100';
+    } while (messages.length > 0);
 });
 
 bot.on('message', function (user, userID, channelID, message, event) {
-	if (message.substring(0, 1) == '!') {
-		var args = message.substring(1).split(' ');
-		var cmd = args[0];
-		switch (cmd) {
-			case 'test':
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+        switch (cmd) {
+            case 'test':
 
-				//invokes the function hello with the param "RPC" on the python server
-				client.invoke("hello", "RPC", function(error, res, more) {
-					console.log(res);
-					bot.sendMessage({
-						to: channelID,
-						message: 'It works!' + res
-					});
-				});
+                //invokes the function hello with the param "RPC" on the python server
+                client.invoke("hello", "RPC", function(error, res, more) {
+                    console.log(res);
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'It works!' + res
+                    });
+                });
 
-				break;
-		}
-	}
+                break;
+        }
+    }
 });
 
