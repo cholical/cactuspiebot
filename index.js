2d241bf3ce17248b3b46d895146aaebe3fb514ed
macvogelsang <macvogelsang@gmail.com>
2017-11-05 12:04:06 -0500

    added fetchText

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var sync = require('sync-request');
 var fs = require('fs');
 var _ = require('lodash');
 var auth = require('./auth.json');
+var latestMessages = require('./latest.json');
 var zerorpc = require("zerorpc");
 
 var discordApi = 'https://discordapp.com/api/';
@@ -22,7 +23,6 @@ var port = 8000;
 var client = new zerorpc.Client();
 client.connect("tcp://" + host + ":" + port);
 
-/*
 bot.on('ready', function (event) {
     console.log('Connected');
     console.log('Logged in as: ');
@@ -37,59 +37,15 @@ bot.on('ready', function (event) {
         }
     }
 
-    var messages = [];
-    var writers = {};
-    var authors = [];
-
-    var counter = 0;
-    for (var i = 0; i < channels.length; i++) {
-        options.url = discordApi + 'channels/' + channels[i] + '/messages?limit=100';
-        console.log(options.url);
-        while (true) {
-            var res = sync('get', options.url, options);
-            messages = JSON.parse(res.getBody().toString());
-            counter += messages.length;
-            console.log("GET " + messages.length + " messages: " + counter + "!");
-            _.forEach(messages, function (message) {
-                if (writers[message.author.id] == undefined) {
-                    writers[message.author.id] = fs.openSync(dataStore + message.author.id + '.txt', 'w');
-                    authors.push(message.author)
-                }
-                //split current line on sentences based on punctuation followed by at least one space
-     			var lines = message.content.replace(/([.?!])\s+(?=[a-zA-Z\d])/g, "$1|").split("|")
-     			_.forEach(lines, function(line){
-     				//for each sentence, write it as a new line
-                	fs.writeSync(writers[message.author.id], line + '\n');
-     			});
-            });
-            if (messages.length > 0) {
-                options.url = discordApi + 'channels/' + channels[i] + '/messages?before=' + messages[messages.length - 1].id + '&limit=100';
-            } else {
-                break;
-            }
-        }
-    }
-
-    for (var property in writers) {
-        if (writers.hasOwnProperty(property)) {
-            fs.closeSync(writers[property]);
-        }
-    }
-
-    _.forEach(authors, function(author) {
-    	console.log("Author map")
-    	console.log(author.id, author.username)
-    })
-
+    fetchText(options, channels);
 });
-*/
+
 bot.on('message', function (user, userID, channelID, message, event) {
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
         switch (cmd) {
             case 'test':
-
                 //invokes the function hello with the param "RPC" on the python server
                 client.invoke("hello", "RPC", function(error, res, more) {
                     console.log(res);
@@ -129,3 +85,53 @@ bot.on('message', function (user, userID, channelID, message, event) {
     }
 });
 
+function fetchText(options,channels){
+
+    var messages = [];
+    var writers = {};
+    var authors = [];
+    var firstTime = true;
+
+    var counter = 0;
+    for (var i = 0; i < channels.length; i++) {
+        options.url = discordApi + 'channels/' + channels[i] + '/messages?limit=100';
+        console.log(options.url);
+        while (true) {
+            var res = sync('get', options.url, options);
+            messages = JSON.parse(res.getBody().toString());
+            counter += messages.length;
+            console.log("GET " + messages.length + " messages: " + counter + "!");
+
+            _.forEach(messages, function (message) {
+                if (writers[message.author.id] == undefined) {
+                    writers[message.author.id] = fs.openSync(dataStore + message.author.id + '.txt', 'w');
+                    authors.push(message.author)
+                }
+                //split current line on sentences based on punctuation followed by at least one space
+     			var lines = message.content.replace(/([.?!])\s+(?=[a-zA-Z\d])/g, "$1|").split("|")
+     			_.forEach(lines, function(line){
+     				//for each sentence, write it as a new line
+                	fs.writeSync(writers[message.author.id], line + '\n');
+     			});
+            });
+            if (messages.length > 0) {
+                options.url = discordApi + 'channels/' + channels[i] + '/messages?before=' + messages[messages.length - 1].id + '&limit=100';
+            } else {
+                break;
+            }
+        }
+    }
+
+    for (var property in writers) {
+        if (writers.hasOwnProperty(property)) {
+            fs.closeSync(writers[property]);
+        }
+    }
+
+    _.forEach(authors, function(author) {
+    	console.log("Author map")
+    	console.log(author.id, author.username)
+    })
+
+
+}
