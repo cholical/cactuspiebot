c8c3376cb31eba89aaa1c1a2dbc2d0ac534fc98a
cholical <ronding@live.unc.edu>
2017-11-04 20:29:09 -0400

    Ronald's changes

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var Discord = require('discord.io');
 var request = require('request');
+var sync = require('sync-request');
+var fs = require('fs');
+var _ = require('lodash');
 var auth = require('./auth.json');
 
 var bot = new Discord.Client({
@@ -12,6 +15,30 @@ bot.on('ready', function (event) {
 	console.log('Logged in as: ');
 	console.log(bot.username + ' - ' + bot.id);
 
+
+	var options = {
+		url: 'https://discordapp.com/api/channels/220307845483069440/messages?limit=100',
+		headers: {
+			'User-Agent':'request',
+			'Authorization': bot.internals.token,
+			'Content-type': 'application/json'
+		}
+	}
+
+	var messages = [];
+	var firstTime = true;
+	var done = true;
+
+	do {
+		var res = sync('get', options.url, options);
+		console.log(res);
+		messages = JSON.parse(res.getBody('utf8'));
+		_.forEach(messages, function (message) {
+			var line = message.author.username + ' (' + message.timestamp + ') ' + ': ' + message.content + '\n';
+			fs.appendFile('messages.txt', line);
+		});
+		options.url = 'https://discordapp.com/api/channels/220307845483069440/messages?before=' + messages[messages.length - 1].id + '&limit=100';
+	} while (messages.length > 0);
 });
 
 bot.on('message', function (user, userID, channelID, message, event) {
