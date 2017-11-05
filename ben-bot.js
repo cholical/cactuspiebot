00508120d2bf53711d4df69f4b056de7dfb20ebe
cholical <ronding@live.unc.edu>
2017-11-05 11:41:13 -0500

    Added bot servers

---
+++ ben-bot.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var Discord = require('discord.io');
+var auth = require('./auth.json');
+var bodyParser = require('body-parser');
+var app = express();
+var port = parseInt(process.argv[2]);
+var botauth = require('./botauth.json');
+
+var channelID = '376604648863170560';
+
+var bot = new Discord.Client({
+    token: botauth.benToken,
+    autorun: true
+});
+
+
+bot.on('ready', function (event) {
+    console.log('Connected');
+    console.log('Logged in as: ');
+    console.log(bot.username + ' - ' + bot.id);
+});
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.json());
+
+
+app.post('/message', function (req, res) {
+	var message = req.body.message;
+	bot.sendMessage({
+        to: channelID,
+        message: message
+    });
+	res.sendStatus(200);
+});
+
+app.listen(port, function () {
+	console.log("Server running on port " + port + ".");
+});
