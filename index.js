2be7f7435fe9d62358275af3450c6e7b0538aa4b
macvogelsang <macvogelsang@gmail.com>
2017-11-06 04:57:22 +0000

    added support for direct bot messageing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var sync = require('sync-request');
 var fs = require('fs');
 var _ = require('lodash');
 var auth = require('./auth.json');
+var botinfo = require('./botconfig.json');
 var zerorpc = require("zerorpc");
 var exec = require('child_process').exec;
 
@@ -15,8 +16,6 @@ var channels = ["220307845483069440", "110114161098248192"];
 //var channels = ["110114161098248192"];
 
 var childProcesses = [];
-var botIds = [];
-var botToAuthor = {};  //obj that maps bot ids to their corresponding author
 
 var bot = new Discord.Client({
     token: auth.token,
@@ -35,11 +34,12 @@ bot.on('ready', function (event) {
     console.log('Logged in as: ');
     console.log(bot.username + ' - ' + bot.id);
 
-    var botsAvailable = ['ronald-bot.js', 'mac-bot.js', 'keenan-bot.js', 'asher-bot.js', 'ben-bot.js'];
-    for (var i = 0; i < botsAvailable.length; i++) {
+    for (var key in botinfo) {
+        if (!botinfo.hasOwnProperty(key)) continue;
+        var botobj = botinfo[key];
+
         try {
-            var botPort = port + 1 + i;
-            var cmd = 'node ' + botsAvailable[i] + ' ' + botPort;
+            var cmd = 'node ' + botobj.script + ' ' + botobj.port;
             console.log(cmd);
             var childProcess = exec(cmd, function (err, stdout, stderr) {
                 if (err) {
@@ -49,7 +49,6 @@ bot.on('ready', function (event) {
             });
             childProcesses.push(childProcess);
             childProcess.stdout.pipe(process.stdout);
-
         } catch (err) {
             console.log(err);
         }
@@ -79,13 +78,19 @@ bot.on('message', function (user, userID, channelID, message, event) {
     //if message mentions a bot, query that bot and send result as message
     var mentionId = getMentionId(message);
     if (mentionId != ""){
-        if (botIds.includes(mentionId)) {
+        if (botinfo[mentionId] != undefined) {
             //query that bot
-            client.invoke("readModel", botToAuthor[mentionId], 1, function(error, res, more) {
+            client.invoke("readModel", botinfo[mentionId].modelUID, 1, function(error, res, more) {
                 console.log(res);
-                bot.sendMessage({
-                    to: channelID,
-                    message: res[0]
+                request.post('http://127.0.0.1:' + botinfo[mentionId].port  + '/message', {
+                    form: {
+                        channelID: channelID,
+                        message: res[0]
+                    }
+                }, function (err, httpResponse, body) {
+                    if (err) {
+                        console.log(err);
+                    }
                 });
             });
         }
@@ -98,9 +103,9 @@ bot.on('message', function (user, userID, channelID, message, event) {
         if (args[1] != undefined) {
             modelUID = getMentionId(args[1]);
             console.log("model id found", modelUID, args[1], message);
-            if (botIds.includes(modelUID)){
+            if (botinfo[modelUID] != undefined){
                 //need to map this id to an actual author id
-                modelUID = botToAuthor[modelUID];
+                modelUID = botinfo[modelUID].modelUID;
             } // otherwise, this is directly mentioning an author
         }
         switch (cmd) {
@@ -128,7 +133,7 @@ bot.on('message', function (user, userID, channelID, message, event) {
             case 'query':
                 client.invoke("readModel", modelUID, 1, function(error, res, more) {
                     console.log(res);
-                    request.post('http://127.0.0.1:8001/message', {
+                    request.post('http://127.0.0.1:' + getBot(modelUID).port  + '/message', {
                         form: {
                             channelID: channelID,
                             message: res[0]
@@ -217,6 +222,17 @@ function getMentionId(string){
         mentionId = match[1];
     }
     return mentionId;
+}
+
+function getBot(modelUID) {
+    for (var key in botinfo) {
+        if (!botinfo.hasOwnProperty(key)) continue;
+        var botobj = botinfo[key];
+        if (botobj.modelUID == modelUID) {
+            return botobj
+        }
+    }
+    return null
 }
 
 process.on('exit', function () {
